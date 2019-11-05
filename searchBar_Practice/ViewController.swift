1ef2ec537df29ad7444d9210191aafa0c48bb3ca
jporras1 <javi.jr2150@gmail.com>
2019-11-05 12:24:02 -0600

    Initial Commit.
    Added the search bar and all implementation.
    
    Used a generic string array to filter through and then printed filtered array to the console.

--- searchBar_Practice/ViewController.swift
+++ searchBar_Practice/ViewController.swift
@@ -9,12 +9,63 @@
 import UIKit
 
 class ViewController: UIViewController {
+    var groceries = [
+        "pizza",
+        "bananas",
+        "apples",
+        "milk",
+        "cheese",
+        "bread"
+    ]
+    var filteredGroceries = [String]()
+    
+    var searchBar: UISearchBar!
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        view.backgroundColor = .gray
+        navigationController?.navigationBar.barTintColor = .darkGray
+        configureSearchBarButton()
     }
-
-
+    @objc func showSearchBar(){
+        print("Configuring Search Bar")
+        configureSearchBar(shouldShow: true)
+    }
+    func configureSearchBar(shouldShow: Bool) {
+        if shouldShow{
+            searchBar = UISearchBar()
+            searchBar.delegate = self
+            searchBar.sizeToFit()
+            searchBar.showsCancelButton = true
+            searchBar.becomeFirstResponder()
+            searchBar.tintColor = .white
+            
+            navigationItem.rightBarButtonItem = nil
+            navigationItem.titleView = searchBar
+        }else{
+            navigationItem.titleView = nil
+            configureSearchBarButton()
+        }
+    }
+    func configureSearchBarButton() {
+        navigationItem.rightBarButtonItem = UIBarButtonItem(barButtonSystemItem: .search, target: self, action: #selector(showSearchBar))
+        navigationItem.rightBarButtonItem?.tintColor = .white
+    }
 }
 
+
+// MARK: - UISearchBarDelegate
+extension ViewController: UISearchBarDelegate {
+    func searchBarCancelButtonClicked(_ searchBar: UISearchBar) {
+        configureSearchBar(shouldShow: false)
+    }
+    
+    func searchBar(_ searchBar: UISearchBar, textDidChange searchText: String) {
+        if searchText == "" || searchBar.text == nil {
+            view.endEditing(true)
+        } else {
+            filteredGroceries = groceries.filter({ $0.range(of: searchText.lowercased()) != nil })
+            print(filteredGroceries)
+        }
+    }
+}
